fb317ffe9571fdac1b5368f46bee49fe9141a3bf
igauravsehrawat <igauravsehrawat@gmail.com>
2014-01-02 08:15:39 +0530

    updating user 1.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -37,6 +37,7 @@ describe "signin" do
 		it {should have_title(user.name)}
 		it {should have_link('Profile', href:user_path(user))}
 		it {should have_link('Sign out', href:signout_path)}
+		it { should have_link('Settins', href:edit_user_path(user))}
 		it {should_not have_link('Sign in', href: signin_path)}
 
 		describe "with invalid information" do
