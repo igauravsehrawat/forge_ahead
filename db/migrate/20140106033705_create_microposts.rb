4af3dc4bc6fc14beb670c3f25943aa5df411079f
igauravsehrawat <igauravsehrawat@gmail.com>
2014-01-15 16:52:35 +0530

    drop_table micropost fix

--- db/migrate/20140106033705_create_microposts.rb
+++ db/migrate/20140106033705_create_microposts.rb
@@ -1,6 +1,6 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
-  	drop_table :microposts
+#  	drop_table :microposts
     create_table :microposts do |t|
       t.string :content
       t.integer :user_id
